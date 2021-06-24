354cbcf92b5b75c34b8fb4d747a81d09d94324a3
ronel1409 <ronelmichael14@gmail.com>
2021-06-24 08:16:59 +0100

    second commit

--- Login Page/login.js
+++ Login Page/login.js
@@ -1,11 +1,28 @@
 //Login
-const name = document.getElementsById('fname').toLowerCase();
-const password = document.getElementsById('inputPassword3')
+const userName = document.getElementsById('email').toLowerCase();
+const password = document.getElementsById('inputPassword3');
+
+const user = {
+    userName = userName,
+    password = password
+}
+
+fetch("locahost:3000").then(() => (res, req) {
+    if (user > 0) {
+        res.status(400)
+    } else {
+        return user = null
+    }
+}). catch (error) {
+    return errror().res.status(401)
+}
 
 
-const Login = () => {
-    if (username === username && password === password) {
-        return `Welcome ${name}`;
+const LoginUser = () => {
+    if (userName === db.username && password === db.password) {
+        return `Welcome ${Username}`;
+    } else if (!password && !userName) {
+        return `User not found`
     } else {
         return `error logging in`
     }
