e7182eb3ca783b2d52de675f4395c47f488d5f59
ronel1409 <ronelmichael14@gmail.com>
2021-06-24 13:13:52 +0100

    completed

--- Login Page/login.js
+++ Login Page/login.js
@@ -1,38 +1,60 @@
 //Login
- 
-    function validateform(){  
-        var name=document.myform.email.value;  
-        var password=document.myform.password.value;  
+
+function validateform(){  
+    //formValidation
+        var email = document.myform.email.value;  
+        var password = document.myform.password.value;  
           
-        if (name==null || name==""){  
+        if (email == null || email == ""){  
           alert("Email can't be blank");  
           return false;  
-        }else if(password.length<6){  
+        } else if(password.length < 6){  
           alert("Password must be at least 6 characters long.");  
           return false;  
-          }  
-        }   
+          } else if (password.length == "") {
+              alert("Please type in password")
+          } 
+
+      //Email & password validation
+            if (email == "") {  
+            email.innerHTML =  
+            " <i class='bi bi-check-circle'></i> Please enter your email";  
+            status=false;  
+            } else {  
+            email.innerHTML = " <i class='bi bi-check-circle'></i>";  
+            status = true;  
+            }  
+            if (password < 6) {  
+            password.innerHTML =  
+            " <i class='bi bi-check-circle'></i> Password must be at least 6 char long";  
+            status = false;  
+            } else {  
+            password.innerHTML = " <i class='bi bi-check-circle'></i>";  
+            }  
+            return status;  
+    }
+      
 
 
-// //RESTApi simultion
-// fetch("https://example.com").then((res, req) => {
-//     if (user > 0) {
-//         res.status(400)
-//     } else {
-//         return user = null
-//     }
-// }).catch((error) => {
-//     return error().res.status(401)
-// })
+    //RESTApi simultion
+    fetch("https://example.com").then((res, req) => {
+        if (user > 0) {
+            res.status(400)
+        } else {
+            return user = null
+        }
+    }).catch((error) => {
+        return error().res.status(401)
+    })
 
-// //login
-// const LoginUser = () => {
-//     if (userName === db.username && password === db.password) {
-//         return `Welcome ${Username}`;
-//     } else if (!password && !userName) {
-//         return `User not found`
-//     } else {
-//         return `error logging in`
-//     }
-// }
+    //login
+    const LoginUser = () => {
+        if (userName === db.username && password === db.password) {
+            return `Welcome ${Username}`;
+        } else if (!password && !userName) {
+            return `User not found`
+        } else {
+            return `error logging in`
+        }
+    }
 
